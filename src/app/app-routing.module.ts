ffddf7efec09113e4f47ee95f0469604549f866c
Viktor Tiutiun <vvtiutiun@googlemail.com>
2020-05-14 21:35:00 +0300

    Added routes for rubrics of bottom menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LandMarketPageComponent } from './land-market-page/land-market-page.component';
+import { AgrarianReformPageComponent } from './agrarian-reform-page/agrarian-reform-page.component';
+import { AbroadPageComponent } from './abroad-page/abroad-page.component';
+import { ViewPageComponent } from './view-page/view-page.component';
+import { EuropeonExperiencePageComponent } from './europeon-experience-page/europeon-experience-page.component';
 
 
 const routes: Routes = [
   {
     path: '', component: MainLayoutComponent, children: [
+      { path: '', redirectTo: '/', pathMatch: 'full' },
+      { path: '', component: HomePageComponent },
+      { path: 'landmarket', component: LandMarketPageComponent },
+      { path: 'agrarionreform', component: AgrarianReformPageComponent },
+      { path: 'abroad', component: AbroadPageComponent },
+      { path: 'view', component: ViewPageComponent },
+      { path: 'europeonexperience', component: EuropeonExperiencePageComponent }
     ]
   }
 
