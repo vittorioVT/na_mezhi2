ba2588732f50be08ae32cc0d0bd410badd03aff7
Viktor Tiutiun <vvtiutiun@googlemail.com>
2020-05-16 20:04:34 +0300

    Created interface user for post data to server

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { User } from '../shared/interfaces';
 
 @Component({
   selector: 'app-login-page',
@@ -27,6 +28,13 @@ export class LoginPageComponent implements OnInit {
     if (this.form.invalid) {
       return;
     }
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    }
+
+
+
   }
 
 }
