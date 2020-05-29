3f07d0ba042c64868a78bcc8fc967d0ad878946a
Viktor Tiutiun <vvtiutiun@googlemail.com>
2020-05-29 13:26:24 +0300

    Created update() metod for posts

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,21 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PostService } from 'src/app/shared/post.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { switchMap } from 'rxjs/operators';
 import { Post } from '../shared/interfaces';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy {
 
   form: FormGroup;
+  post: Post;
+  submitted = false;
+  uSub: Subscription;
 
   constructor(private route: ActivatedRoute,
-    private postsService: PostService) { }
+    private postsService: PostService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.route.params.pipe(
@@ -23,10 +28,34 @@ export class EditPageComponent implements OnInit {
         return this.postsService.getById(params['id']);
       })
     ).subscribe((post: Post) => {
+      this.post = post;
       this.form = new FormGroup({
         title: new FormControl(post.title, Validators.required),
         text: new FormControl(post.text, Validators.required)
       })
     })
   }
+
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+    this.submitted = true;
+
+    this.uSub = this.postsService.update({
+      ...this.post,
+      text: this.form.value.text,
+      title: this.form.value.title,
+    }).subscribe(() => {
+      this.submitted = false;
+      this.router.navigate(['/admin','dashboard']);
+      })
+  }
+
+  ngOnDestroy() {
+    if (this.uSub) {
+      this.uSub.unsubscribe();
+    }
+  }
+
 }
--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -21,7 +21,7 @@ export class AuthInterceptor implements HttpInterceptor{
     }
     return next.handle(req)
       .pipe(tap(() => {
-        console.log('Intercept');
+        //console.log('Intercept');
       }),
 
       catchError((error: HttpErrorResponse) => {
