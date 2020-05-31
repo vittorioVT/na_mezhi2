be38a10a0d096c48fafb7eef2bb03a5cec64f66d
Viktor Tiutiun <vvtiutiun@googlemail.com>
2020-05-31 15:31:34 +0300

    Created page for single post

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Post } from '../admin/shared/interfaces';
+import { PostService } from '../shared/post.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-post-page',
@@ -8,10 +12,15 @@ import { Post } from '../admin/shared/interfaces';
 })
 export class PostPageComponent implements OnInit {
 
+  posts$: Observable<Post>
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+    private postsService: PostService) { }
 
   ngOnInit(): void {
+    this.posts$ = this.route.params
+      .pipe(switchMap((params: Params) => {
+        return this.postsService.getById(params['id']);
+      }))
   }
-
 }
